79a3485a4ed7c38440b1e8c887e19be9d62574fe
Dawn Haynes <haynestdawn@yahoo.com>
2018-11-07 11:46:43 -0500

    Update PalindromeNumber/Program.cs

--- PalindromeNumber/Program.cs
+++ PalindromeNumber/Program.cs
@@ -4,12 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PalindromeNumber
+namespace ConsoleApp4
 {
     class Program
     {
         static void Main(string[] args)
+
         {
+            Console.WriteLine("Palindrome Number Game");
+            int count = 1;
+            while (true)
+            {
+                try
+                {
+                    string palendrome = "";
+                    string numberReversed = "";
+
+                    char[] palendromeArray = count++.ToString().ToCharArray();
+                   
+
+                    foreach (char item in palendromeArray)
+                    {
+                        palendrome += item;
+                    }
+
+                    Array.Reverse(palendromeArray);
+
+                    foreach (char items in palendromeArray)
+                    {
+                        numberReversed += items;
+                    }
+
+                    if (palendrome == numberReversed)
+                    {
+                        Console.WriteLine("Your number is a Palendrome! " + numberReversed + " Press enter to see the next Palendrome.");
+                        Console.ReadLine();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("General Exception Caught");
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
